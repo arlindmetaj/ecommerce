d2805e34d2d5eae56a5f05e979158ea1974c1b41
almotech <123456>
2021-12-07 18:05:37 +0100

    Search Bar

--- lib/Screens/Search/search_screen.dart
+++ lib/Screens/Search/search_screen.dart
@@ -26,15 +26,18 @@ class _SearchScreenState extends State<SearchScreen> {
               child: SizedBox(
                 height: 35,
                 child: Padding(
-                  padding: const EdgeInsets.all(6.0),
+                  padding: const EdgeInsets.only(bottom: 5, left: 10),
                   child: TextFormField(
+                    onTap: (){
+                      showSearch(context: context, delegate: CustomSearchDelegate());
+                    },
                     decoration: InputDecoration(
                       border: InputBorder.none,
                       hintText: DemoLocalization.of(context)!
                           .getTranslatedValue("search_here"),
                       hintStyle: TextStyle(
                           color: Colors.grey.shade500,
-                          fontSize: 11
+                          fontSize: 12
                       ),
                     ),
                   ),
@@ -43,19 +46,86 @@ class _SearchScreenState extends State<SearchScreen> {
             ),
           ],
         ),
-        actions: [
-          Container(
-            margin: const EdgeInsets.only(right: 10),
-            child: const Icon(
-              Icons.search,
-              color: Colors.grey,
-            ),
-          )
+        actions: const [
+           Icon(Icons.search),
         ],
       ),
       body: const SingleChildScrollView(
         child: SearchProductsView(),
       ),
+    );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<String> searchTerms = [
+    "Apple",
+    "Banana",
+    "Pear",
+    "Watermelons",
+    "Oranges",
+    "Blueberries",
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back,),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
     );
   }
 }
